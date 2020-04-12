5644d1b26c25301c79aeb997c055ffee62001635
DangThi <19127276@student.hcmus.edu.vn>
2020-04-12 18:01:02 +0700

    SHA-512(include project)

---
+++ SHA-512/SHA-512/Hashing_Pass.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "header.h"
+
+using std::string;
+using std::cout;
+using std::endl;
+
+int main(int argc, char* argv[])
+{
+    cout << "input password:";
+        string input1;
+    std::cin >> input1;
+    string output2 = sha512(input1);
+    string input2 = "thi";
+    
+    string output1 = sha512(input2);
+
+    cout << "sha512('" << input2 << "'):" << output1 << endl;
+    if (output1 == output2)
+        cout << "Accept";
+    else
+        cout << "Deny";
+    return 0;
+}

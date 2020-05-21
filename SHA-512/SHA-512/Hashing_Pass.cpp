36abbf7fb3646c09ca4faa1828e09a5c788300bd
DangThi <19127276@student.hcmus.edu.vn>
2020-05-21 19:04:15 +0700

    sha-512

--- SHA-512/SHA-512/Hashing_Pass.cpp
+++ SHA-512/SHA-512/Hashing_Pass.cpp
@@ -11,13 +11,14 @@ int main(int argc, char* argv[])
 {
     cout << "input password:";
         string input1;
-    std::cin >> input1;
-    string output2 = sha512(input1);
-    string input2 = "thi";
-    
-    string output1 = sha512(input2);
+    std::cin >> input1;// nhập pass
+    string output1 = sha512(input1);
 
-    cout << "sha512('" << input2 << "'):" << output1 << endl;
+    string input2 = "thi";//vd pass dc lưu trữ là "Thi"
+    string output2 = sha512(input2);
+
+    cout << "sha512('" << input1 << "'):" << output1 << endl;
+    cout << "sha512('" << input2 << "'):" << output2 << endl;
     if (output1 == output2)
         cout << "Accept";
     else
